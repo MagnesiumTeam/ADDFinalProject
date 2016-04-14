ffa4678931aa3f8bf28556f5cc0476d6c605ce17
MagnesiumTeam <themagnesiumteam@gmail.com>
2016-04-14 06:02:54 -0500

    Input & Output wrappers added

---
+++ InputWrapper.v
@@ -0,0 +1,9 @@
+/*
+Name: The Magnesium Team (Olanrewaju Ibironke, Su Hui Tan, Anitha Ramesh Puranik, Meghna Chittajallu)
+Date: April 14, 2016
+Project: Input Wrapper
+*/
+module InputWrapper();
+
+
+endmodule
---
+++ OutputWrapper.v
@@ -0,0 +1,9 @@
+/*
+Name: The Magnesium Team (Olanrewaju Ibironke, Su Hui Tan, Anitha Ramesh Puranik, Meghna Chittajallu)
+Date: April 14, 2016
+Project: Output Wrapper
+*/
+module OutputWrapper();
+
+
+endmodule
